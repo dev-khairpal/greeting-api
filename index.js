bb22438eb42fdae5c1cd52b975d504c710c81e0e
dev-khairpal <133212101+dev-khairpal@users.noreply.github.com>
2025-03-07 13:03:48 +0530

    fix for cors - try 2

--- index.js
+++ index.js
@@ -11,13 +11,17 @@ const PORT = process.env.PORT || 6000
 const app = express();
 app.use(
     cors({
-      origin: ["http://localhost:5175", "https://greeting-client-xi.vercel.app/",
-        "greeting-client-git-main-dev-khairpals-projects.vercel.app","greeting-client-hy855blw4-dev-khairpals-projects.vercel.app"
-      ], 
+      origin: [
+        "http://localhost:5175",
+        "https://greeting-client-xi.vercel.app",
+        "https://greeting-client-git-main-dev-khairpals-projects.vercel.app",
+        "https://greeting-client-hy855blw4-dev-khairpals-projects.vercel.app"
+      ],
       methods: ["GET", "POST"],
       allowedHeaders: ["Content-Type"],
     })
   );
+  
 app.use(express.json());
 
 
